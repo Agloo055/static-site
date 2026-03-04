bfbfad0249efbe1b5b31d2cab00c5bfd1ebbe6dd
Agloo055 <jacksonoberman@gmail.com>
2026-03-04 15:22:17 -0800

    add more content

--- src/main.py
+++ src/main.py
@@ -8,6 +8,10 @@ def main():
     copy_directory('static','public')
 
     generate_page('content/index.md','template.html','public/index.html')
+    generate_page('content/blog/glorfindel/index.md','template.html','public/blog/glorfindel/index.md')
+    generate_page('content/blog/tom/index.md','template.html','public/blog/tom/index.md')
+    generate_page('content/blog/majesty/index.md','template.html','public/blog/majesty/index.md')
+    generate_page('content/contact/index.md','template.html','public/contact/index.md')
 
 
 
